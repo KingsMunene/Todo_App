923c19fdc66925e2460e6d0fda20e657df03a126
KingsMunene <munenekings2@gmail.com>
2023-11-05 13:57:49 +0300

    Entity data class for the todo app

---
+++ app/src/main/java/com/example/todoapp/data/Todo.kt
@@ -0,0 +1,13 @@
+package com.example.todoapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// A table for the todo app
+@Entity
+data class Todo(
+    val title: String,
+    val description: String,
+    val isDone: Boolean,
+    @PrimaryKey val todoId: Int
+)
